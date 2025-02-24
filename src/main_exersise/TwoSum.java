05fb47221b4b2ea0f49f48053b07df3d751ad12f
thaovy1994 <letranthaovy1326@gmail.com>
2025-02-24 19:32:38 +0700

    Update excercise

--- src/main_exersise/TwoSum.java
+++ src/main_exersise/TwoSum.java
@@ -1,4 +1,19 @@
 package main_exersise;
 
 public class TwoSum {
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    System.out.println("Index: [" + i + "," + j + "]");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No pair found!");
+    }
 }
